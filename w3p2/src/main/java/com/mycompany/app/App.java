1e61976ca71804875066363d96cfdf4c395b54a4
Salida Maharjan <salidamaharjan@outlook.com>
2025-12-19 22:33:06 -0600

    Solve word count

--- w3p2/src/main/java/com/mycompany/app/App.java
+++ w3p2/src/main/java/com/mycompany/app/App.java
@@ -1,10 +1,42 @@
 package com.mycompany.app;
 
-/**
- * Hello world!
- */
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+
 public class App {
+
+    public static Map<String, Integer> wordCount() {
+        Map<String, Integer> wordCountMap = new HashMap<>();
+        wordCountMap = readFile("/Users/salida/code/JavaOOP-Project/w3p2/src/main/java/com/mycompany/app/App.java");
+        return wordCountMap;
+    }
+
+    public static Map<String, Integer> readFile(String fileName) {
+        File file = new File(fileName);
+        Map<String, Integer> wordCountMap = new HashMap<>();
+        try (Scanner fileReader = new Scanner(file)) {
+            System.out.println("\n--- Reading from file---");
+            while (fileReader.hasNextLine()) {
+                String data = fileReader.nextLine();
+                String[] words = data.split(" ");
+
+                for (String word : words) {
+                    Integer count = wordCountMap.get(word);
+                    if (count == null) {
+                        wordCountMap.put(word, 1);
+                    } else {
+                        wordCountMap.put(word, count + 1);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Could not read task file.");
+        }
+        return wordCountMap;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(wordCount());
     }
 }
