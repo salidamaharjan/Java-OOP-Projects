f43af91f47bfb120afe8863489eae9302e0b42be
Salida Maharjan <salidamaharjan@outlook.com>
2025-12-21 05:13:57 -0600

    Use Regex to exclude non alphabetic character

--- w3p2/src/main/java/com/mycompany/app/App.java
+++ w3p2/src/main/java/com/mycompany/app/App.java
@@ -7,7 +7,7 @@ import java.util.*;
 public class App {
 
     public static Map<String, Integer> wordCount() {
-        Map<String, Integer> wordCountMap = new HashMap<>();
+        Map<String, Integer> wordCountMap;
         wordCountMap = readFile("/Users/salida/code/JavaOOP-Project/w3p2/src/main/java/com/mycompany/app/App.java");
         return wordCountMap;
     }
@@ -18,15 +18,18 @@ public class App {
         try (Scanner fileReader = new Scanner(file)) {
             System.out.println("\n--- Reading from file---");
             while (fileReader.hasNextLine()) {
-                String data = fileReader.nextLine();
+                String data = fileReader.nextLine().trim().strip().toLowerCase();
                 String[] words = data.split(" ");
-
                 for (String word : words) {
-                    Integer count = wordCountMap.get(word);
+                    System.out.println("in for: " + word);
+                    String strippedWord = stringTrim(word);
+                    System.out.println("in for: stripped word " + strippedWord);
+
+                    Integer count = wordCountMap.get(strippedWord);
                     if (count == null) {
-                        wordCountMap.put(word, 1);
+                        wordCountMap.put(strippedWord, 1);
                     } else {
-                        wordCountMap.put(word, count + 1);
+                        wordCountMap.put(strippedWord, count + 1);
                     }
                 }
             }
@@ -34,6 +37,18 @@ public class App {
             System.out.println("Could not read task file.");
         }
         return wordCountMap;
+    }
+
+    public static String stringTrim(String word) {
+        String[] wordSplit = word.split("");
+        StringBuilder stripTrimWord = new StringBuilder();
+        for (String s : wordSplit) {
+            if (!s.matches(".*[^A-Za-z0-9 ].*") && !s.matches(".*\\p{Punct}.*")) {
+                stripTrimWord.append(s);
+            }
+        }
+        System.out.println(stripTrimWord);
+        return stripTrimWord.toString();
     }
 
     public static void main(String[] args) {
