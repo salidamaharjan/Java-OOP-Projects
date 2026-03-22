7a10258da245fa90fd30a3cc33475c95fcc1fec5
Salida Maharjan <salidamaharjan@outlook.com>
2026-03-21 22:10:31 -0500

    Get all student from DB

--- w4p1/src/main/java/com/mycompany/app/App.java
+++ w4p1/src/main/java/com/mycompany/app/App.java
@@ -3,6 +3,7 @@ package com.mycompany.app;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class App {
     public static void main(String[] args) {
@@ -12,14 +13,22 @@ public class App {
         String password = "password";
 
         try(Connection conn = DriverManager.getConnection(url,user,password)){
-            PreparedStatement preparedStatement = conn.prepareStatement("INSERT INTO student (name, major) VALUES (?, ?)");
-            preparedStatement.setString(1, "Aman");
-            preparedStatement.setString(2, "MCS" );
-            preparedStatement.executeUpdate();
-//            System.out.println(resultSet);
-            System.out.println("Student added");
-//            resultSet.close();
-            preparedStatement.close();
+            // Add student to the DB
+//            PreparedStatement preparedStatement = conn.prepareStatement("INSERT INTO student (name, major) VALUES (?, ?)");
+//            preparedStatement.setString(1, "Aman");
+//            preparedStatement.setString(2, "MCS" );
+//            preparedStatement.executeUpdate();
+//            System.out.println("Student added");
+//            preparedStatement.close();
+            PreparedStatement preparedStatement1 = conn.prepareStatement("Select * FROM student");
+            ResultSet resultSet = preparedStatement1.executeQuery();
+            System.out.println("Name of Students ↓");
+            while(resultSet.next()){
+                String name = resultSet.getString("name");
+                System.out.println("Name: " + name);
+            }
+            resultSet.close();
+            preparedStatement1.close();
         }catch(Exception e){
             System.err.println("Connection failed!");
             System.err.println("Error: " + e.getMessage());
